9a4eaffc66760d40dfc367ccb3465065b018b8e7
wendy <mizukilee@gmail.com>
2022-11-25 10:10:12 -0500

    commit tree ea3868f62a2032ec251f4ddb810125b2edfe7bb6

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+// DOMContentLoaded Event 
+document.addEventListener("DOMContentLoaded", function() {
+    const element = document.getElementById("text") 
+    console.log(element.textContent = 'This is really cool!')
+    
+  });
+  
+ 
